7b7839aef99b5cdbe7bbe1c329a190550b7ca198
05012000aA <anhkhoacttv@gamil.com>
2022-02-20 10:50:48 +0700

    useState counter basic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { useState } from "react";
 function App() {
+	const [counter, setCounter] = useState(0);
+
+	const handleOnClick = () => {
+		setCounter(counter + 1);
+	};
+
 	return (
 		<div>
-			<h1>Khoa123</h1>
+			<h1>USE STATE</h1>
+			<h1>{counter}</h1>
+			<button onClick={handleOnClick}>click me!</button>
 		</div>
 	);
 }
